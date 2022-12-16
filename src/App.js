9e9b70d5bcfd976d391452ef85a6b5d3bab0dd9b
Tamara Eaton <Tamara.Eaton@bounteous.com>
2022-12-16 09:04:54 -0700

    adding untracked files

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import { render } from "react-dom";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { StrictMode, useState } from "react";
+import ThemeContext from "./ThemeContext";
+import SearchParams from "./SearchParams";
+import Details from "./Details";
+
+const App = () => {
+  const theme = useState("darkpink");
+  return (
+    <StrictMode>
+      <ThemeContext.Provider value={theme}>
+        <BrowserRouter>
+          <header>
+            <Link to="/">Adopt Me!</Link>
+          </header>
+          <Routes>
+            <Route path="/details/:id" element={<Details />} />
+            <Route path="/" element={<SearchParams />} />
+          </Routes>
+        </BrowserRouter>
+      </ThemeContext.Provider>
+    </StrictMode>
+  );
+};
+
+render(<App />, document.getElementById("root"));
---
+++ src/Results.js
@@ -0,0 +1,25 @@
+import React from "react";
+import Pet from "./Pet";
+
+const Results = ({ pets }) => {
+  return (
+    <div className="grid gap-4 grid-cols-2">
+      {!pets.length ? (
+        <h1>No Pets Found</h1>
+      ) : (
+        pets.map((pet) => (
+          <Pet
+            name={pet.name}
+            animal={pet.animal}
+            breed={pet.breed}
+            images={pet.images}
+            location={`${pet.city}, ${pet.state}`}
+            id={pet.id}
+          />
+        ))
+      )}
+    </div>
+  );
+};
+
+export default Results;
